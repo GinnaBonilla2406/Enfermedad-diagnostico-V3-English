281b61b000eaee4cdbe88379fa6d1c39b593d6eb
GinnaBonilla2406 <g2406b@gmail.com>
2017-04-18 16:35:47 -0500

    config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+	/* Esto son mi parámetro de mi base de datos. */
+ 
+	$Server = "localhost";
+    $user = "root";
+    $key = "";
+    $bd = "bd_enfermedad";
+
+    //Puede colocar s o n minúsculas para ver diferentes impresiones de prueba de SQL y otras características.
+	 //$sn_diagnostico_clinico = "s";
+	 //$sn_escribir_log = "s";
+ ?>
